4225bff4f65e4f21862affb0bcea5f59287ce77a
tnauss <nausst@gmail.com>
2015-01-29 11:19:27 +0100

    Update - include comments

--- src/dm-es-12-02_shiny_html_no_markdown/server.R
+++ src/dm-es-12-02_shiny_html_no_markdown/server.R
@@ -1,4 +1,24 @@
-library(shiny)
+# Shiny app to illustrate interactive visualization using shiny
+#
+# Copyright (C) 2014 Thomas Nauss
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Please send any comments, suggestions, criticism, or (for our sake) bug
+# reports to admin@environmentalinformatics-marburg.de
+
+#### General setttings #########################################################
 
 if(Sys.info()["user"] == "shiny"){
   in_path1 <- "/srv/shiny-server/moc/msc-data-management/data_procd/"
@@ -11,9 +31,11 @@ if(Sys.info()["user"] == "shiny"){
   in_path3 <- paste0(getwd(), "/data_procd/spatial_data/")
 }
 
-packages <- c("raster", "rgdal", "latticeExtra", "caret", "gridExtra")
+packages <- c("shiny", "raster", "rgdal", "latticeExtra", "caret", "gridExtra")
 lapply(packages, library, character.only = TRUE)
 
+
+#### Load necessary datasets ###################################################
 wb <- read.table(paste0(in_path1, "wb-db_gnipc_co2_1960-2013.csv"), 
                  header = TRUE, sep = ",")
 cntry <- read.table(paste0(in_path2, "wb-db_country_classes.csv"), 
@@ -25,6 +47,8 @@ wbc$Region <- droplevels(wbc$Region)
 wbc$co2_log <- log(wbc$co2)
 wbc$gni_log <- log(wbc$gni)
 
+
+#### Initialize shiny server ###################################################
 shinyServer(function(input, output) {
   
   wbc <- wbc[complete.cases(wbc),]
--- src/dm-es-12-02_shiny_html_no_markdown/ui.R
+++ src/dm-es-12-02_shiny_html_no_markdown/ui.R
@@ -1,6 +1,28 @@
+# Shiny app to illustrate interactive visualization using shiny
+#
+# Copyright (C) 2014 Thomas Nauss
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Please send any comments, suggestions, criticism, or (for our sake) bug
+# reports to admin@environmentalinformatics-marburg.de
+
+#### General setttings #########################################################
 library(shiny)
 
 
+#### Initialize shiny user interface ###########################################
 shinyUI(fluidPage(
   
   titlePanel("Histogram"),
@@ -12,8 +34,9 @@ shinyUI(fluidPage(
       
       sliderInput(inputId = "bw_adjust", label = "Bandwidth adjustment:",
                   min = 0.2, max = 2, value = 1, step = 0.2,
-                  animate = animationOptions(interval = 1000, loop = TRUE, 
-                                             playButton = NULL, pauseButton = NULL))
+                  animate = animationOptions(
+                    interval = 1000, loop = TRUE, 
+                    playButton = NULL, pauseButton = NULL))
     ),
     mainPanel(plotOutput("histo")
     )
